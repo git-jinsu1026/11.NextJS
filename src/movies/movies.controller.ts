a9c6d9a84da5b5b0f2bcd723791f0d069476d255
박진수 <parkjinsu@bagjinsuui-MacBookPro.local>
2021-04-11 12:30:06 +0900

    create, getid, getall

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,21 +1,25 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 //4행에서 /movies url로 해주어야한다. 즉 엔트리포인트 역할(라우터 역할)
 @Controller('movies')
 export class MoviesController {
+    //import하는 부분도 수동으로 되는 게 아니라 실제적으로 작성을 해줘야한다.
+    constructor(private readonly moviesService: MoviesService) { }
 
     // GET의 이부분은 express.js의 어플리케이션 라우터
     @Get()
-    getAll() {
-        return "this will return movies";
+    getAll(): Movie[] {
+        return this.moviesService.getAll();
     }
 
     //서치의 위치가 밑에 있는 GET보다 위에 있어야한다. 그래야만 잘 작동이 된다 
     // /search?year=2000
-    @Get("search")
-    search(@Query("year") searchingYear: string) {
-        return `We are seacrhing for a moive with a made after: ${searchingYear}`
-    }
+    // @Get("search")
+    // search(@Query("year") searchingYear: string) {
+    //     return `We are seacrhing for a moive with a made after: ${searchingYear}`
+    // }
 
     //키포인트는 id의 값을 받는다
     //nextjs는 요청을 하려면 우리가 작성해줘야한다.
@@ -24,18 +28,18 @@ export class MoviesController {
 
     //@Param('id') 값을 가져온다.
     @Get("/:id")
-    getOne(@Param('id') movieId: string) {
-        return `this will return one Movie ${movieId}`
+    getOne(@Param('id') movieId: string): Movie {
+        return this.moviesService.getOne(movieId);
     }
 
     @Post()
     create(@Body() movieData) {
-        return movieData
+        return this.moviesService.create(movieData)
     }
     //url 에서 id를ㄹ 가져와서 movieId라는 string 타입의 변수에 저장
     @Delete("/:id")
     remove(@Param('id') movieId: string) {
-        return `This vill delete a moive with the id :${movieId}`
+        return this.moviesService.deleteOne(movieId);
     }
 
     @Put('/:id')
